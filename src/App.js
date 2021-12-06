d6cc21a365c0345eb4ff4a907a2d18e4330c42ca
calvin <s11710044@student.unklab.ac.id>
2021-12-06 13:45:05 +0800

    ui ecommerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header class="section-header">
+         <section class="header-main border-bottom">
+            <div class="container">
+         <div class="row align-items-center">
+            <div class="col-lg-2 col-4">
+               <h5>UI E-Commerce</h5>
+            </div>
+            <div class="col-lg-6 col-sm-12">
+               <form action="#" class="search">
+                     <div class="input-group w-100">
+                        <input type="text" class="form-control" placeholder="Search" />
+                        <button type="button" class="btn btn-primary">Search</button>
+                        <div class="input-group-append">
+                        </div>
+                     </div>
+               </form>
+            </div> 
+            <div class="col-lg-4 col-sm-6 col-12">
+               <div class="widgets-wrap float-md-end">
+                     <div class="widget-header  me-3">
+                        <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-shopping-cart"></i></a>
+                        <span class="badge badge-pill badge-danger notify">0</span>
+                     </div>
+                     <div class="widget-header icontext">
+                        <a href="#" class="icon icon-sm rounded-circle border"><i class="fa fa-user"></i></a>
+                     </div>
+               </div> 
+            </div> 
+         </div> 
+            </div> 
+         </section> 
+         <nav class="navbar navbar-main navbar-expand-lg navbar-light border-bottom">
+         <div class="container">
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+               <ul class="navbar-nav">
+               <button type="button" class="btn btn-primary">Home</button>
+               <button type="button" class="btn btn-danger">About</button>
+               <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" type="button" class="btn btn-warning" data-bs-toggle="dropdown" aria-expanded="false">Pilihan</a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                     <li><a class="dropdown-item" href="#">Sofa Mahal</a></li>
+                     <li><a class="dropdown-item" href="#">Sofa Murah </a></li>
+                     <li><hr class="dropdown-divider" /></li>
+                     <li><a class="dropdown-item" href="#">Sofa Besar </a></li>
+                     <li><a class="dropdown-item" href="#">Sofa Kecil </a></li>
+                     <li><hr class="dropdown-divider" /></li>
+                     <li><a class="dropdown-item" href="#">Sofa Kayu </a></li>
+                     <li><a class="dropdown-item" href="#">Sofa Besi </a></li>
+                  </ul>
+               </li>
+               </ul>
+            </div> 
+         </div> 
+         </nav>
+         </header>
+        
+         <section class="section-main bg padding-y">
+         <div class="container">
+         <div class="row">
+            <aside class="col-md-3">
+               <nav class="card">
+               </nav>
+            </aside>
+            </div>
+            
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card">
+        
+        <td>
+        <tr>
+        <a class="d-grid gap-2 d-md-block">
+          <button type="button" class="btn btn-success">new</button>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="249" height="50"><path fill-rule="evenodd" d="M2 4a1 1 0 100-2 1 1 0 000 2zm3.75-1.5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zM3 8a1 1 0 11-2 0 1 1 0 012 0zm-1 6a1 1 0 100-2 1 1 0 000 2z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path fill-rule="evenodd" d="M6.736 4C4.657 4 2.5 5.88 2.5 8.514c0 3.107 2.324 5.96 4.861 8.12a29.66 29.66 0 004.566 3.175l.073.041.073-.04c.271-.153.661-.38 1.13-.674.94-.588 2.19-1.441 3.436-2.502 2.537-2.16 4.861-5.013 4.861-8.12C21.5 5.88 19.343 4 17.264 4c-2.106 0-3.801 1.389-4.553 3.643a.75.75 0 01-1.422 0C10.537 5.389 8.841 4 6.736 4zM12 20.703l.343.667a.75.75 0 01-.686 0l.343-.667zM1 8.513C1 5.053 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262a31.146 31.146 0 01-5.233 3.576l-.025.013-.007.003-.002.001-.344-.666-.343.667-.003-.002-.007-.003-.025-.013A29.308 29.308 0 0110 20.408a31.147 31.147 0 01-3.611-2.632C3.8 15.573 1 12.332 1 8.514z"></path></svg>
+      </a>
+      </tr>
+      </td>
+
+        
+        <img src="https://id-live-05.slatic.net/p/3d6baac12db9d53f9a05b7fec415397a.jpg_720x720q80.jpg_.webp" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <ul class="nav justify-content-center">
+          <h5 class="card-title">Sofa Kayu</h5>
+          </ul>
+         
+         <tr>
+          <h1><td> &#10031; &#10031; &#10031; &#10031; &#10031;
+            <button type="button" class="btn btn-outline-success">$150</button>
+                <button type="button" class="btn btn-outline-info">Buy</button>
+          </td></h1>
+        </tr>
+
+        </div>
+      </div>
+    </div>
+
+    
+    <div class="col">
+      <div class="card">
+      <td>
+      <tr>
+        <a class="d-grid gap-2 d-md-block">
+          <button type="button" class="btn btn-success">new</button>
+        
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="249" height="50"><path fill-rule="evenodd" d="M2 4a1 1 0 100-2 1 1 0 000 2zm3.75-1.5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zM3 8a1 1 0 11-2 0 1 1 0 012 0zm-1 6a1 1 0 100-2 1 1 0 000 2z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path fill-rule="evenodd" d="M6.736 4C4.657 4 2.5 5.88 2.5 8.514c0 3.107 2.324 5.96 4.861 8.12a29.66 29.66 0 004.566 3.175l.073.041.073-.04c.271-.153.661-.38 1.13-.674.94-.588 2.19-1.441 3.436-2.502 2.537-2.16 4.861-5.013 4.861-8.12C21.5 5.88 19.343 4 17.264 4c-2.106 0-3.801 1.389-4.553 3.643a.75.75 0 01-1.422 0C10.537 5.389 8.841 4 6.736 4zM12 20.703l.343.667a.75.75 0 01-.686 0l.343-.667zM1 8.513C1 5.053 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262a31.146 31.146 0 01-5.233 3.576l-.025.013-.007.003-.002.001-.344-.666-.343.667-.003-.002-.007-.003-.025-.013A29.308 29.308 0 0110 20.408a31.147 31.147 0 01-3.611-2.632C3.8 15.573 1 12.332 1 8.514z"></path></svg>
+      </a>
+      </tr>
+      </td>
+        
+        <img src="https://dekorasirumahjati.com/wp-content/uploads/2019/11/sofa-tamu-minimalis-20-e1593433413738-510x324.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <ul class="nav justify-content-center">
+          <h5 class="card-title">Sofa Besi </h5>
+          </ul>
+          
+          <tr>
+            <h1><td> &#10031; &#10031; &#10031; &#10031; &#10031;
+              <button type="button" class="btn btn-outline-success">$200</button>
+                <button type="button" class="btn btn-outline-info">Buy</button>
+            </td></h1>
+          </tr>
+
+        </div>
+      </div>
+    </div>
+
+    
+      <div class="col">
+        <div class="card">
+        <td>
+        <tr>
+          <a class="d-grid gap-2 d-md-block">
+            <button type="button" class="btn btn-success">new</button>
+          
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="249" height="50"><path fill-rule="evenodd" d="M2 4a1 1 0 100-2 1 1 0 000 2zm3.75-1.5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zM3 8a1 1 0 11-2 0 1 1 0 012 0zm-1 6a1 1 0 100-2 1 1 0 000 2z"></path></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><path fill-rule="evenodd" d="M6.736 4C4.657 4 2.5 5.88 2.5 8.514c0 3.107 2.324 5.96 4.861 8.12a29.66 29.66 0 004.566 3.175l.073.041.073-.04c.271-.153.661-.38 1.13-.674.94-.588 2.19-1.441 3.436-2.502 2.537-2.16 4.861-5.013 4.861-8.12C21.5 5.88 19.343 4 17.264 4c-2.106 0-3.801 1.389-4.553 3.643a.75.75 0 01-1.422 0C10.537 5.389 8.841 4 6.736 4zM12 20.703l.343.667a.75.75 0 01-.686 0l.343-.667zM1 8.513C1 5.053 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262a31.146 31.146 0 01-5.233 3.576l-.025.013-.007.003-.002.001-.344-.666-.343.667-.003-.002-.007-.003-.025-.013A29.308 29.308 0 0110 20.408a31.147 31.147 0 01-3.611-2.632C3.8 15.573 1 12.332 1 8.514z"></path></svg>
+        </a></tr></td>
+          
+          <img src="https://kursicafebar.com/wp-content/uploads/2018/12/kursi-tamu-sofa.jpg" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <ul class="nav justify-content-center">
+            <h5 class="card-title">Sofa Mewah</h5>
+            </ul>
+            
+            <tr>
+              <h1><td> &#10031; &#10031; &#10031; &#10031; &#10031;
+                <button type="button" class="btn btn-outline-success">$250</button>
+                <button type="button" class="btn btn-outline-info">Buy</button>
+              </td></h1>
+            </tr>
+          </div>
+        </div>
+      </div>
+
+         </div>
+         </div> 
+         </section>
     </div>
   );
 }
